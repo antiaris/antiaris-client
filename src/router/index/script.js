650b278968430786d0c68ee87bd309cdc3a6f8fc
yinyong02 <yinyong02@baidu.com>
2016-08-01 20:19:26 +0800

    fixed entry

---
+++ src/router/index/script.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2016 antiaris.xyz
+ * script.js
+ *
+ * changelog
+ * 2016-08-01[20:11:42]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+System.import('client:src/component/nav/nav-script.js').then(module => {
+    console.log(module)
+}).catch(err => {
+    console.error(err)
+})
