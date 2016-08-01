afa43ec51730b5d4294c123f258421ec2af3fe4f
yinyong02 <yinyong02@baidu.com>
2016-08-01 20:15:11 +0800

    reconstruction

--- src/router.js
+++ src/router.js
@@ -10,38 +10,8 @@
  * @since 0.1.0
  */
 'use strict';
-import fs from 'fs';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
-import Nav from './component/nav/nav.jsx';
-
-import nunjucks from 'nunjucks';
-
-nunjucks.configure({autoescape: false});
+import indexRouter from './router/index/index.jsx';
 
 export default (router) => {
-    router.get('/', async function (ctx, next) {
-        const {add} = ctx;
-
-        const tpl = await new Promise((resolve, reject) => {
-            fs.readFile(__dirname + '/view/index.html', 'utf-8', (err, content) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(content);
-                }
-            });
-        });
-
-        const content = ReactDOMServer.renderToString(<Nav add={add}/>);
-
-        ctx.body = nunjucks.renderString(tpl,{
-            content,
-            css: `<link rel="stylesheet" href="/s?` + ctx.comboCss().join() + `">`,
-            script: `<script src="/s?` + ctx.comboScript().join() +`"></script>`
-        })
-
-        next();
-    });
+    router.get('/', indexRouter);
 };
---
+++ src/router/index/script.jsx
@@ -0,0 +1,16 @@
+/**
+  * Copyright (C) 2016 antiaris.xyz
+  * script.js
+  *
+  * changelog
+  * 2016-08-01[20:11:42]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+import Nav from './nav.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Nav/>, document.querySelector('#react-dom'));
